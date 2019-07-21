f055d4a7a32d43d59c2cc01e2db9dc5091a97b30
thanhnd3996 <ndt4081@gmail.com>
2019-07-21 20:52:20 +0700

    Update

--- train_val_split_2018.py
+++ train_val_split_2018.py
@@ -3,23 +3,17 @@ from sklearn.model_selection import train_test_split
 
 train_labels = pd.read_csv("ISIC2018_Task3_Training_GroundTruth.csv")
 
-
-# print(train_labels.head())
-
-
 def diagnosis(row):
     return row[row == 1].index[0]
 
 
 train_labels['diagnosis'] = train_labels.apply(diagnosis, axis=1)
-print(train_labels.shape)
 train_labels.drop(columns=['MEL', 'NV', 'BCC', 'AKIEC', 'BKL', 'DF', 'VASC'], inplace=True)
+print(train_labels.groupby('diagnosis').count())
 
 train, val = train_test_split(train_labels, test_size=0.2)
-print(train.groupby('diagnosis').count)
-print(val.groupby('diagnosis').count)
-# print(train)
-# print(test)
+print(train.groupby('diagnosis').count())
+print(val.groupby('diagnosis').count())
 
 train.to_csv('train.csv', index=False)
 val.to_csv('val.csv', index=False)
