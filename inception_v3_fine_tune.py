b1bb9fa17999379a956606735078f72ea5711146
thanhnd3996 <ndt4081@gmail.com>
2019-07-23 11:53:03 +0700

    Add training file

--- inception_v3_fine_tune.py
+++ inception_v3_fine_tune.py
@@ -28,24 +28,38 @@ valid_generator = val_datagen.flow_from_directory("./dataset/val_images/",
 
 # define model architecture
 # load base_model
-base_model = InceptionV3(include_top=False, weights=None, input_shape=(299, 299, 3))
+base_model = InceptionV3(include_top=False, weights='imagenet', input_shape=(299, 299, 3))
 last = base_model.output
 
 x = Dense(2048, activation='relu')(last)
+x = Dropout(0.4)(x)
 x = GlobalAveragePooling2D()(x)
-preds = Dense(7, activation='softmax')(x)	
+preds = Dense(7, activation='softmax')(x)
 model = Model(input=base_model.input, output=preds)
 model.summary()
 
-# Compile model
+
+# transfer learning
+for layer in base_model.layers:
+    layer.trainable = False
+
 sgd = SGD(lr=0.1, clipnorm=5.)
+model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=['accuracy'])
+model.fit_generator(train_generator, steps_per_epoch=8012 // 8, validation_data=valid_generator,
+					validation_steps=2003 // 8, epochs=20, verbose=1)
+
+
+# fine tune
+for layer in model.layers[:229]:
+    layer.trainable = False
+for layer in model.layers[229:]:
+    layer.trainable = True
 
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 # Save the model with best weights
-checkpointer = ModelCheckpoint('./checkpoint/inception_v3_1.h5',
-                               verbose=1, save_best_only=True,
+checkpointer = ModelCheckpoint('./checkpoint/inception_v3_fine_tune.h5',
+                               verbose=1, save_weights_only=True,
                                monitor='val_acc', mode="max")
-
 # train model
 model.fit_generator(train_generator, steps_per_epoch=8012 // 8, validation_data=valid_generator,
                     validation_steps=2003 // 8, epochs=150, verbose=1, callbacks=[checkpointer])
