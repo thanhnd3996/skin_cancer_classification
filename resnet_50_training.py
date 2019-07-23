0418c5b7785efb13a380fa6067c1f4bd33a178d3
thanhnd3996 <ndt4081@gmail.com>
2019-07-23 16:55:58 +0700

    add training file

--- resnet_50_training.py
+++ resnet_50_training.py
@@ -1,9 +1,12 @@
-from keras.applications.inception_v3 import InceptionV3
+from keras.applications.resnet50 import ResNet50
 from keras.callbacks import ModelCheckpoint
 from keras.layers import Dense, GlobalAveragePooling2D
 from keras.models import Model
 from keras.optimizers import SGD
 from keras.preprocessing import image
+
+batch_size = 8
+epochs = 1000
 
 # data pre-processing
 train_gen_args = dict(rotation_range=30,
@@ -21,14 +24,14 @@ val_datagen = image.ImageDataGenerator(**val_gen_args)
 
 # load image
 train_generator = train_datagen.flow_from_directory("./dataset/train_images/",
-                                                    target_size=(299, 299), batch_size=8)
+                                                    target_size=(224, 224), batch_size=batch_size)
 
 valid_generator = val_datagen.flow_from_directory("./dataset/val_images/",
-                                                  target_size=(299, 299), batch_size=8)
+                                                  target_size=(224, 224), batch_size=batch_size)
 
 # define model architecture
 # load base_model
-base_model = InceptionV3(include_top=False, weights=None, input_shape=(299, 299, 3))
+base_model = ResNet50(include_top=False, weights=None, input_shape=(224, 224, 3))
 last = base_model.output
 
 x = Dense(2048, activation='relu')(last)
@@ -42,10 +45,11 @@ sgd = SGD(lr=0.1, clipnorm=5.)
 
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 # Save the model with best weights
-checkpointer = ModelCheckpoint('./checkpoint/inception_v3_1.h5',
+checkpointer = ModelCheckpoint('./checkpoint/resnet_50.h5',
                                verbose=1, save_best_only=True,
+                               save_weights_only=True,
                                monitor='val_acc', mode="max")
 
 # train model
-model.fit_generator(train_generator, steps_per_epoch=8012 // 8, validation_data=valid_generator,
-                    validation_steps=2003 // 8, epochs=150, verbose=1, callbacks=[checkpointer])
+model.fit_generator(train_generator, steps_per_epoch=8012 // batch_size, validation_data=valid_generator,
+                    validation_steps=2003 // batch_size, epochs=epochs, verbose=1, callbacks=[checkpointer])
