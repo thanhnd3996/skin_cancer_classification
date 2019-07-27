c9a2e9b341d52bfd6fdf99f5eca63c1cd3fbee19
thanhnd3996 <ndt4081@gmail.com>
2019-07-28 01:16:49 +0700

    update feature_extractor

--- feature_extractor.py
+++ feature_extractor.py
@@ -7,19 +7,24 @@ from keras.applications.resnet50 import ResNet50, preprocess_input
 from keras.layers import GlobalAveragePooling2D, Dense, Dropout
 from keras.preprocessing import image
 import xgboost as xgb
+from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
 
 # parameter and file path
 num_classes = 7
-labels = ['AKIEC', 'BCC', 'BKL', 'DF', 'MEL', 'NV', 'VASC']
 trained_resnet_weights = "./checkpoint/resnet_50.h5"
 dataset = "./dataset/"
 train_set = "./dataset/train_images/"
 val_set = "./dataset/val_images"
 
+# create encoder for label
+labels = ['AKIEC', 'BCC', 'BKL', 'DF', 'MEL', 'NV', 'VASC']
+label_encoder = LabelEncoder()
+
 # create model and load weight
 base_model = ResNet50(include_top=False, weights=None, input_shape=(224, 224, 3))
 last = base_model.output
+
 layer = Dense(2048, activation='relu')(last)
 layer = Dropout(0.4)(layer)
 layer = GlobalAveragePooling2D()(layer)
@@ -43,15 +48,12 @@ for train_path in train_paths:
     X_train.append(features_reduce)
 
     # generate one hot vector for label
-    targets = np.zeros(num_classes)
     label = train_path.split(os.path.sep)[-2]
-    for i in labels:
-        if i == label:
-            targets[labels.index(i)] = 1
-    y_train.append(targets)
+    y_train.append(label)
 
 X_train = np.array(X_train)
-y_train = np.array(y_train, np.uint8)
+y_train = np.array(y_train, np)
+y_train = label_encoder.transform(y_train)
 
 X_test = []
 y_test = []
@@ -66,15 +68,12 @@ for val_path in val_paths:
     features_reduce = features.squeeze()
     X_test.append(features_reduce)
 
-    targets = np.zeros(num_classes)
     label = val_path.split(os.path.sep)[-2]
-    for i in labels:
-        if i == label:
-            targets[labels.index(i)] = 1
-    y_test.append(targets)
+    y_test.append(label)
 
 X_test = np.array(X_test)
 y_test = np.array(y_test, np.uint8)
+y_test = label_encoder.transform(y_test)
 
 print('Training and making predictions')
 clf = xgb.XGBClassifier(max_depth=15, learning_rate=0.1, n_estimators=200)
