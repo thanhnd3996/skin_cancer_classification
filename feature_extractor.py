8c848b4f74c1ad159ece677f71b19ab493a3bcce
thanhnd3996 <ndt4081@gmail.com>
2019-07-29 06:36:50 +0700

    feature_extractor.py

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,36 +1,52 @@
 import os
 
 import numpy as np
+import xgboost as xgb
 from imutils import paths
 from keras import Model
-from keras.applications.resnet50 import ResNet50, preprocess_input
-from keras.layers import GlobalAveragePooling2D, Dense, Dropout
+from keras.applications.inception_v3 import InceptionV3, preprocess_input
+from keras.applications.resnet50 import ResNet50
+from keras.layers import GlobalAveragePooling2D, Dense
 from keras.preprocessing import image
-import xgboost as xgb
-from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
 
 # parameter and file path
 num_classes = 7
+labels = ['AKIEC', 'BCC', 'BKL', 'DF', 'MEL', 'NV', 'VASC']
 trained_resnet_weights = "./checkpoint/resnet_50.h5"
+trained_inception_weights = "./checkpoint/inception_v3.h5"
 dataset = "./dataset/"
 train_set = "./dataset/train_images/"
 val_set = "./dataset/val_images"
 
-# create encoder for label
-labels = ['AKIEC', 'BCC', 'BKL', 'DF', 'MEL', 'NV', 'VASC']
-label_encoder = LabelEncoder()
-
 # create model and load weight
+print("Loading Resnet 50 ...")
 base_model = ResNet50(include_top=False, weights=None, input_shape=(224, 224, 3))
 last = base_model.output
 
 layer = Dense(2048, activation='relu')(last)
-layer = Dropout(0.4)(layer)
 layer = GlobalAveragePooling2D()(layer)
 model = Model(inputs=base_model.input, outputs=layer)
-model.summary()
 model.load_weights(trained_resnet_weights)
+
+print("Loading Inception V3")
+inception_model = InceptionV3(include_top=False, weights=None, input_shape=(299, 299, 3))
+inception_last = inception_model.output
+
+inception_layer = Dense(2048, activation='relu')(inception_last)
+inception_layer = GlobalAveragePooling2D()(inception_layer)
+model_2 = Model(inputs=inception_model.input, outputs=inception_layer)
+model_2.load_weights(trained_inception_weights)
+
+
+def read_image(target_size, img_path):
+    img = image.load_img(img_path, target_size=target_size)
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
+
 
 # load only validation images
 X_train = []
@@ -38,14 +54,15 @@ y_train = []
 
 train_paths = sorted(list(paths.list_images(train_set)))
 for train_path in train_paths:
-    img = image.load_img(train_path, target_size=(224, 224))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
+    x_resnet = read_image((224, 224), train_path)
+    x_inception = read_image((299, 299), train_path)
 
-    features = model.predict(x)
-    features_reduce = features.squeeze()
-    X_train.append(features_reduce)
+    ft_1 = model.predict(x_resnet)
+    ft_2 = model_2.predict(x_inception)
+    features_reduce_1 = ft_1.squeeze()
+    features_reduce_2 = ft_2.squeeze()
+    ft_vector = np.add(features_reduce_1, features_reduce_2)
+    X_train.append(ft_vector)
 
     # generate one hot vector for label
     label = train_path.split(os.path.sep)[-2]
@@ -53,27 +70,33 @@ for train_path in train_paths:
 
 X_train = np.array(X_train)
 y_train = np.array(y_train, np)
-y_train = label_encoder.transform(y_train)
+train_lb_encoder = LabelEncoder()
+train_lb_encoder = train_lb_encoder.fit(y_train)
+y_train = train_lb_encoder.transform(y_train)
 
 X_test = []
 y_test = []
 val_paths = sorted(list(paths.list_images(val_set)))
-for val_path in val_paths:
-    img = image.load_img(val_path, target_size=(224, 224))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
 
-    features = model.predict(x)
-    features_reduce = features.squeeze()
-    X_test.append(features_reduce)
+for val_path in val_paths:
+    x_resnet = read_image((224, 224), val_path)
+    x_inception = read_image((299, 299), val_path)
+
+    ft_1 = model.predict(x_resnet)
+    ft_2 = model_2.predict(x_inception)
+    features_reduce_1 = ft_1.squeeze()
+    features_reduce_2 = ft_2.squeeze()
+    ft_vector = np.add(features_reduce_1, features_reduce_2)
+    X_test.append(ft_vector)
 
     label = val_path.split(os.path.sep)[-2]
     y_test.append(label)
 
 X_test = np.array(X_test)
-y_test = np.array(y_test, np.uint8)
-y_test = label_encoder.transform(y_test)
+y_test = np.array(y_test)
+val_lb_encoder = LabelEncoder()
+val_lb_encoder = val_lb_encoder.fit(y_test)
+y_test = val_lb_encoder.transform(y_test)
 
 print('Training and making predictions')
 clf = xgb.XGBClassifier(max_depth=15, learning_rate=0.1, n_estimators=200)
