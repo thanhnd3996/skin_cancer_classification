f8a23e1a1d65eed65e631bc3f593a1f80f4508d5
thanhnd3996 <ndt4081@gmail.com>
2019-08-10 00:25:42 +0700

    print acc for nv and non nv

--- task_1/ft_extract_task1.py
+++ task_1/ft_extract_task1.py
@@ -4,10 +4,10 @@ import numpy as np
 from imutils import paths
 from keras import Model
 from keras.applications.inception_v3 import InceptionV3, preprocess_input
-from keras.applications.resnet50 import preprocess_input
-from keras.layers import GlobalAveragePooling2D
+from keras.layers import GlobalAveragePooling2D, Dense
 from keras.preprocessing import image
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix
 
 # parameter and file path
 num_classes = 2
@@ -24,6 +24,7 @@ inception_model = InceptionV3(include_top=False, weights=None, input_shape=(299,
 inception_last = inception_model.output
 
 inception_fcn = GlobalAveragePooling2D()(inception_last)
+inception_fcn = Dense(num_classes, activation='softmax')(inception_fcn)
 model_1 = Model(inputs=inception_model.input, outputs=inception_fcn)
 model_1.load_weights(trained_task2_model, by_name=True)
 
@@ -40,50 +41,74 @@ def feature_extractor(img_path):
     x_1 = read_image((299, 299), img_path)
 
     ft_1 = model_1.predict(x_1)
-    ft_vector = ft_1.squeeze()
+    # ft_vector = ft_1.squeeze()
 
-    return ft_vector
-
-
-def create_train_data():
-    X_train = []
-    y_train = []
-    train_paths = sorted(list(paths.list_images(train_set)))
-    for train_path in train_paths:
-        train_ft = feature_extractor(train_path)
-        X_train.append(train_ft)
-        label = train_path.split(os.path.sep)[-2]
-        y_train.append(label)
-
-    X_train = np.array(X_train)
-    y_train = np.array(y_train, np)
-    train_lb_encoder = LabelEncoder()
-    train_lb_encoder = train_lb_encoder.fit(y_train)
-    y_train = train_lb_encoder.transform(y_train)
-
-    np.save('X_train.npy', X_train)
-    np.save('y_train.npy', y_train)
+    # return ft_vector
+    return ft_1
 
 
-def create_val_data():
-    X_val = []
-    y_val = []
-    val_paths = sorted(list(paths.list_images(val_set)))
-    for val_path in val_paths:
-        val_ft = feature_extractor(val_path)
-        X_val.append(val_ft)
-        label = val_path.split(os.path.sep)[-2]
-        y_val.append(label)
+#
+# def create_train_data():
+#     X_train = []
+#     y_train = []
+#     train_paths = sorted(list(paths.list_images(train_set)))
+#     for train_path in train_paths:
+#         train_ft = feature_extractor(train_path)
+#         X_train.append(train_ft)
+#         label = train_path.split(os.path.sep)[-2]
+#         y_train.append(label)
+#
+#     X_train = np.array(X_train)
+#     y_train = np.array(y_train, np)
+#     train_lb_encoder = LabelEncoder()
+#     train_lb_encoder = train_lb_encoder.fit(y_train)
+#     y_train = train_lb_encoder.transform(y_train)
+#
+#     np.save('X_train.npy', X_train)
+#     np.save('y_train.npy', y_train)
 
-    X_val = np.array(X_val)
-    y_val = np.array(y_val)
+#
+# def create_val_data():
+#     X_val = []
+#     y_val = []
+#     val_paths = sorted(list(paths.list_images(val_set)))
+#     for val_path in val_paths:
+#         val_ft = feature_extractor(val_path)
+#         X_val.append(val_ft)
+#         label = val_path.split(os.path.sep)[-2]
+#         y_val.append(label)
+#
+#     X_val = np.array(X_val)
+#     y_val = np.array(y_val)
+#     val_lb_encoder = LabelEncoder()
+#     val_lb_encoder = val_lb_encoder.fit(y_val)
+#     y_val = val_lb_encoder.transform(y_val)
+#     np.save('X_val.npy', X_val)
+#     np.save('y_val.npy', y_val)
+
+
+def create_test_data():
+    X_test = []
+    y_test = []
+    test_paths = sorted(list(paths.list_images(test_set)))
+    for test_path in test_paths:
+        test_ft = feature_extractor(test_path)
+        X_test.append(test_ft)
+        label = test_path.split(os.path.sep)[-2]
+        y_test.append(label)
+
+    X_test = np.array(X_test)
+    y_test = np.array(y_test)
     val_lb_encoder = LabelEncoder()
-    val_lb_encoder = val_lb_encoder.fit(y_val)
-    y_val = val_lb_encoder.transform(y_val)
-    np.save('X_val.npy', X_val)
-    np.save('y_val.npy', y_val)
+    val_lb_encoder = val_lb_encoder.fit(y_test)
+    y_test = val_lb_encoder.transform(y_test)
+    return X_test, y_test
 
 
 if __name__ == '__main__':
-    create_train_data()
-    create_val_data()
+    # create_train_data()
+    # create_val_data()
+    X_test, y_test = create_test_data()
+    cm = confusion_matrix(y_test, X_test)
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    print(cm.diagonal())
