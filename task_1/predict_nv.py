e36444e5cd7142adbf519c01f9081ea7a5b99399
thanhnd3996 <ndt4081@gmail.com>
2019-08-11 02:16:28 +0700

    predict nv vs non nv

--- task_1/predict_nv.py
+++ task_1/predict_nv.py
@@ -38,41 +38,40 @@ def evaluate():
     model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
     score = model.predict_generator(test_generator, steps=nb_samples)
 
-    # for i in range(0, nb_samples):
-    #     idx = np.argmax(score[i])
-    #     print(idx, score[i][idx])
+    for i in range(0, nb_samples):
+        idx = np.argmax(score[i])
+        print(idx, score[i][idx])
 
     y_pred = np.argmax(score, axis=1)
     cm = confusion_matrix(test_generator.classes, y_pred)
     print(cm.diagonal())
-    # plot_confusion_matrix(cm, idx, normalize=True, title="Normalize confusion matrix")
-    # plt.show()
+    plot_confusion_matrix(cm, idx, normalize=True, title="Normalize confusion matrix")
+    plt.show()
 
 
-#
-# def plot_confusion_matrix(cm, classes,
-#                           normalize=False,
-#                           title='Confusion matrix',
-#                           cmap=plt.cm.Blues):
-#     if normalize:
-#         cm = cm.astype('float') / cm.sum(axis=1, keepdims=True)
-#
-#     plt.imshow(cm, interpolation='nearest', cmap=cmap)
-#     plt.title(title)
-#     plt.colorbar()
-#     tick_marks = np.arange(len(classes))
-#     plt.xticks(tick_marks, classes, rotation=45)
-#     plt.yticks(tick_marks, classes)
-#     fmt = ".2f" if normalize else 'd'
-#     thresh = cm.max() / 2.
-#     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-#         plt.text(j, i, format(cm[i, j], fmt),
-#                  horizontalaligment="center",
-#                  color="white" if cm[i, j] > thresh else "black")
-#         plt.tight_layout()
-#         plt.ylabel("True label")
-#         plt.xlabel("Predicted label")
-#
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1, keepdims=True)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+    fmt = ".2f" if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalaligment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+        plt.tight_layout()
+        plt.ylabel("True label")
+        plt.xlabel("Predicted label")
+
 
 if __name__ == '__main__':
     evaluate()
