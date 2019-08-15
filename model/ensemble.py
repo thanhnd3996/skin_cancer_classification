40bda342b69ab3a97b527aab13fbd182c8cb10c7
thanhnd3996 <ndt4081@gmail.com>
2019-08-15 13:17:16 +0700

    smote for imbalance data

--- model/ensemble.py
+++ model/ensemble.py
@@ -6,9 +6,10 @@ from xgboost import plot_importance
 from sklearn.metrics import accuracy_score
 from sklearn.feature_selection import SelectFromModel
 from sklearn.metrics import confusion_matrix
+from imblearn.over_sampling import SMOTE
 
 # diagnosis
-diagnosis = ['AKIEC', 'BCC', 'BKL', 'DF', 'MEL', 'NV', 'VASC']
+diagnosis = ['NV', 'malignant', 'cancer_2', 'benign']
 
 # load feature and label from file
 X_train = np.load('X_train.npy')
@@ -16,14 +17,18 @@ y_train = np.load('y_train.npy')
 X_val = np.load('X_val.npy')
 y_val = np.load('y_val.npy')
 
+sm = SMOTE(random_state=12, ratio = 1.0)
+X_train_res, y_train_res = sm.fit_sample(X_train, y_train)
+
+
 print('Training and making predictions')
-clf = xgb.XGBClassifier(max_depth=15, learning_rate=0.1, n_estimators=200)
+clf = xgb.XGBClassifier(max_depth=15, learning_rate=0.1, n_estimators=100)
 clf.fit(X_train, y_train)
 
 predictions = clf.predict(X_val)
 accuracy = accuracy_score(y_val, predictions)
 # print("Accuracy: %.2f%%" % (accuracy * 100))
-
+#
 # plot_importance(clf)
 #
 # pyplot.show()
