394e75fd2b4eed0316fbcc00f43050e0ecfa9202
thanhnd3996 <ndt4081@gmail.com>
2019-08-21 19:05:04 +0700

    convert to img

--- data/convert_to_img.py
+++ data/convert_to_img.py
@@ -1,17 +1,20 @@
 import matplotlib
 import numpy as np
-from imutils import paths
 from matplotlib import image
+import os
 
-dataset = "../preprocess/"
-des_dir = "../new_dataset/train/"
+dataset = "../preprocess/val/"
+des_dir = "../new_dataset/val_images/"
 
-img_paths = sorted(list(paths.list_images(dataset)))
-for img_path in img_paths:
-    diagnosis = img_path.split("/")[-2]
-    img_name = img_path.split("/")[-1]
-    img_name = img_name.split(".")[-2]
-    img_name = img_name.split
-    arr = np.load(img_path)
-    img_name = img_name + ".jpg"
-    matplotlib.image.imsave(des_dir + diagnosis + img_name, arr)
+for root, dirs, files in os.walk(dataset):
+
+    for file in files:
+        p = os.path.join(root, file)
+        diagnosis = p.split("/")[-2] + "/"
+        img_name = p.split("/")[-1]
+        img_name = img_name.split(".")[-2]
+        arr = np.load(p)
+        img_name = img_name + ".jpg"
+        if not os.path.exists(des_dir + diagnosis):
+            os.makedirs(des_dir + diagnosis)
+        matplotlib.image.imsave(des_dir + diagnosis + img_name, arr)
