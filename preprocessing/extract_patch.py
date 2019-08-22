8411e790e274c57d1bda370346b4eec5e1db97b7
thanhnd3996 <ndt4081@gmail.com>
2019-08-23 04:11:11 +0700

    extract patch

---
+++ preprocessing/extract_patch.py
@@ -0,0 +1,49 @@
+import cv2
+from imutils import paths
+from sklearn.feature_extraction import image as patch_ex
+from matplotlib import image
+
+PATCH_SIZE = 299
+
+train = "../preprocess/train_images/"
+val = "../preprocess/val_images/"
+new_train = "../new_dataset/train_images/"
+new_val = "../new_dataset/val_images/"
+
+
+def extract_path(dataset, new_dataset, num_per_patch=None):
+    img_paths = sorted(list(paths.list_images(dataset)))
+    for img_path in img_paths:
+        diag = img_path.split("/")[-2]
+        if diag == 'AKIEC':
+            num_per_patch = 20
+        elif diag == 'BCC':
+            num_per_patch = 13
+        elif diag == 'BKL':
+            num_per_patch = 6
+        elif diag == 'DF':
+            num_per_patch = 60
+        elif diag == 'MEL':
+            num_per_patch = 6
+        elif diag == 'NV':
+            num_per_patch = 1
+        elif diag == 'VASC':
+            num_per_patch = 50
+        img_name = img_path.split("/")[-1]
+        img_name = img_name.split(".")[-2]
+
+        img = cv2.imread(img_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        patches = patch_ex.extract_patches_2d(img, (PATCH_SIZE, PATCH_SIZE), num_per_patch)
+        i = 0
+
+        for patch in patches:
+            i += 1
+            rgb_im = cv2.cvtColor(patch, cv2.COLOR_BGR2RGB)
+            image.imsave(new_dataset + diag + "/" + img_name + "_" + str(i) + ".jpg", rgb_im)
+
+
+if __name__ == '__main__':
+    extract_path(train, new_train)
+    extract_path(val, new_val)
